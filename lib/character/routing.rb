64fbfd035b40794a50b22a0f7249bdeed43af2e9
Alex Kravets <alex@slatestudio.com>
2013-07-05 14:44:11 +0300

    redactor support;

--- lib/character/routing.rb
+++ lib/character/routing.rb
@@ -9,6 +9,16 @@ module ActionDispatch::Routing
         match '/login',   to: 'auth#login'
         match '/logout',  to: 'auth#logout'        
 
+        # Register redactor images if module is defined
+        # -------------------------------------------------
+        # I don't really like that this is hardcoded here. It would be nice
+        # to have module option to add it's routes to the /admin scope.
+        if defined?(Character::Redactor)
+          scope '/redactor', :module => "Redactor" do
+            resources :images, only: [:index, :create]
+          end
+        end
+
         get     '/:model_slug(.:format)',         to: 'admin#index'
         get     '/:model_slug/new(.:format)',     to: 'admin#new'
         post    '/:model_slug(.:format)',         to: 'admin#create'
@@ -16,6 +26,7 @@ module ActionDispatch::Routing
         get     '/:model_slug/:id/edit(.:format)',to: 'admin#edit'
         put     '/:model_slug/:id(.:format)',     to: 'admin#update'
         delete  '/:model_slug/:id(.:format)',     to: 'admin#destroy'
+
       end
     end
   end
