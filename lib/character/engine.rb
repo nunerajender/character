4f8a242f61c66e2dd103598bd765814a3f39955d
Alex Kravets <alex@slatestudio.com>
2013-07-12 11:37:11 +0300

    add login background image option;

--- lib/character/engine.rb
+++ lib/character/engine.rb
@@ -6,14 +6,16 @@ module Character
       config.browserid.login.path        = '/admin/login'
       config.browserid.logout.path       = '/admin/logout'
 
-      Character.title              = 'Character'
-      Character.company_logo_image = ''
+      Character.title                  = 'Character'
+      Character.company_logo_image     = 'character-company-logo.png'
+      Character.login_background_image = 'http://images.nationalgeographic.com/exposure/core_media/ngphoto/image/68263_0_1040x660.jpg'
     end
   end
 
   class << self
     attr_accessor :title
     attr_accessor :company_logo_image
+    attr_accessor :login_background_image
 
     def configure(&block)
       block.call(self)
