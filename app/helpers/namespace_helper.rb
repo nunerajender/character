73a7b75b8a7f0db0b484469817a6aecfe3d1e2b7
Roman <roman@slatestudio.com>
2013-08-29 14:20:21 +0300

    Allow puting assets into character folder

--- app/helpers/namespace_helper.rb
+++ app/helpers/namespace_helper.rb
@@ -7,4 +7,12 @@ module NamespaceHelper
     end
   end
 
+
+  def find_asset(*files)
+    files.each do |file|
+      return file if Rails.application.assets.find_asset(file)
+    end
+    raise ArgumentError.new "Cannot find any of: #{files.join(",")}"
+  end
+
 end
