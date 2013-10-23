921618bd97c39ec3b76031ecad1d909e1ef512e6
Maksym Melnyk <maxim@slatestudio.com>
2013-10-23 23:46:49 +0300

    Minor changes

--- test/lib/character/routing_test.rb
+++ test/lib/character/routing_test.rb
@@ -5,35 +5,31 @@ class Character::RoutingTest < ActionDispatch::IntegrationTest
     assert true
   end
 
-  #
-  # Routes for API
-  #
-
-  test "should root to index (api)" do
+  test "api should have root for index" do
     assert_routing({ path: "/admin/Foo", method: "get" }, { controller: "character/api", action: "index", model_slug: "Foo"})
   end
 
-  test "should root to show (api)" do
+  test "api should have root for show" do
     assert_routing({ path: "/admin/Foo/1", method: "get" }, { controller: "character/api", action: "show", model_slug: "Foo", id: "1"})
   end
 
-  test "should root to new (api)" do
+  test "api should have root for new" do
     assert_routing({ path: "/admin/Foo/new", method: "get" }, { controller: "character/api", action: "new", model_slug: "Foo"})
   end
 
-  test "should root to create (api)" do
+  test "api should have root for create" do
     assert_routing({ path: "/admin/Foo", method: 'post' }, { controller: "character/api", action: "create", model_slug: "Foo"})
   end
 
-  test "should root to edit (api)" do
+  test "api should have root for edit" do
     assert_routing({ path: "/admin/Foo/1/edit", method: 'get' }, { controller: "character/api", action: "edit", model_slug: "Foo", id: "1"})
   end
 
-  test "should root to update (api)" do
+  test "api should have root for update" do
     assert_routing({ path: "/admin/Foo/1", method: 'put' }, { controller: "character/api", action: "update", model_slug: "Foo", id: "1"})
   end
 
-  test "should root to destroy (api)" do
+  test "api should have root for destroy" do
     assert_routing({ path: "/admin/Foo/1", method: 'delete' }, { controller: "character/api", action: "destroy", model_slug: "Foo", id: "1"})
   end
 end
