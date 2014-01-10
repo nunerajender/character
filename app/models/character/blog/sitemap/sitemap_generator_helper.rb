03c011eadd18097b84bfe442bb0c123a134c9674
Alex Kravets <alex@slatestudio.com>
2014-01-10 12:41:31 +0200

    fix sitemap generator for blog; add settings variable model;

--- app/models/character/blog/sitemap/sitemap_generator_helper.rb
+++ app/models/character/blog/sitemap/sitemap_generator_helper.rb
@@ -2,9 +2,10 @@ class Character::Blog::Sitemap::SitemapGeneratorHelper
   def self.add_links(sitemap)
     url_helpers = Rails.application.routes.url_helpers
 
-    sitemap.add "http://#{ Character::Blog.domain }#{ url_helpers.blog_index_path }", changefreq: "daily"
+    sitemap.add "http://#{ @blog_domain }#{ url_helpers.blog_index_path }", changefreq: "daily"
+
     Character::Blog::Post.published.each do |post|
-      sitemap.add "http://#{ Character::Blog.domain }#{ url_helpers.blog_post_path(post) }", changefreq: "weekly", lastmod: post.updated_at
+      sitemap.add "http://#{ @blog_domain }#{ url_helpers.blog_post_path(post) }", changefreq: "weekly", lastmod: post.updated_at
     end
   end
 end
---
+++ app/models/character/settings/variable.rb
@@ -0,0 +1,3 @@
+# encoding: UTF-8
+class Character::Settings::Variable
+end
