2ca21665cdd2f10a1f99d5be937fb43bd9afd87d
Alex Kravets <alex@slatestudio.com>
2014-01-29 11:25:40 +0200

    flat page helpers do not require page object, assuming it is in view scope @object

--- app/helpers/flat_page_helper.rb
+++ app/helpers/flat_page_helper.rb
@@ -1,27 +1,35 @@
 module FlatPageHelper
-  def inline_editable(flat_page, template_content_key, placeholder='', default_content='')
-    content = flat_page.template_content[template_content_key]
-    content ||= default_content
+  def inline_editable(template_content_key, placeholder='', default_content='')
+    if @object
+      flat_page = @object
 
-    if @form_action_url
-      """<span class='chr-medium-editor-inline'
-               data-input-name='character_flat_page[template_content][#{ template_content_key }]'
-               data-placeholder='#{ placeholder }'>#{ content }</span>""".html_safe
-    else
-      content.html_safe
+      content = flat_page.template_content[template_content_key]
+      content ||= default_content
+
+      if @form_action_url
+        """<span class='chr-medium-editor-inline'
+                 data-input-name='character_flat_page[template_content][#{ template_content_key }]'
+                 data-placeholder='#{ placeholder }'>#{ content }</span>""".html_safe
+      else
+        content.html_safe
+      end
     end
   end
 
-  def editable(flat_page, template_content_key, placeholder='', default_content='')
-    content = flat_page.template_content[template_content_key]
-    content ||= default_content
+  def editable(template_content_key, placeholder='', default_content='')
+    if @object
+      flat_page = @object
 
-    if @form_action_url
-      """<div class='chr-medium-editor'
-              data-input-name='character_flat_page[template_content][#{ template_content_key }]'
-              data-placeholder='#{ placeholder }'>#{ content }</div>""".html_safe
-    else
-      content.html_safe
+      content = flat_page.template_content[template_content_key]
+      content ||= default_content
+
+      if @form_action_url
+        """<div class='chr-medium-editor'
+                data-input-name='character_flat_page[template_content][#{ template_content_key }]'
+                data-placeholder='#{ placeholder }'>#{ content }</div>""".html_safe
+      else
+        content.html_safe
+      end
     end
   end
 end
