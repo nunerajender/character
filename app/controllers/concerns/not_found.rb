b24f600632954cae09cfc1f3c0226eaaa9359cd8
Alex Kravets <alex@slatestudio.com>
2014-04-29 09:56:41 +0300

    refactor helper;

--- app/controllers/concerns/not_found.rb
+++ app/controllers/concerns/not_found.rb
@@ -3,6 +3,10 @@ module NotFound
 
   included do
     rescue_from Mongoid::Errors::DocumentNotFound do |exception|
+      redirect_or_not_found()
+    end
+
+    def redirect_or_not_found
       redirect = Character::Redirect.where(path: request.path).first
       if redirect
         redirect_to redirect.destination, status: redirect.type
